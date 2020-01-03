163f385024ccc178be9a37785a3e72546fbe703f
wizard <15109929502@163.com>
2020-01-03 15:03:38 +0800

    权限模块简单实现3.1.1(简单获取登录用户信息)

--- server/src/main/java/com/wizard/demo01/server/controller/SysUserController.java
+++ server/src/main/java/com/wizard/demo01/server/controller/SysUserController.java
@@ -1,8 +1,15 @@
 package com.wizard.demo01.server.controller;
 
-import org.springframework.stereotype.Controller;
+import com.google.common.collect.Maps;
+import com.wizard.demo01.common.response.BaseResponse;
+import com.wizard.demo01.common.response.StatusCode;
+import com.wizard.demo01.model.entity.SysUserEntity;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * 用户管理Controller
@@ -11,8 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/sys/user")
-public class SysUserController {
-
-
+public class SysUserController extends AbstractController {
+    /**
+     * 获取当前登录用户详情
+     * @return
+     */
+    @RequestMapping(value = "/info",method = RequestMethod.GET)
+    public BaseResponse currInfo(){
+        BaseResponse response = new BaseResponse(StatusCode.Success);
+        //用来存储user信息
+        Map<String,Object> resMap = Maps.newHashMap();
+        try{
+            //获取当前登录用户的详情
+            SysUserEntity user =(SysUserEntity) SecurityUtils.getSubject().getPrincipal();
+            resMap.put("user",user);
+        }catch (Exception e){
+            return new BaseResponse(StatusCode.Fail);
+        }
+        response.setData(resMap);
+        return response;
+    }
 
 }
---
+++ server/src/main/java/com/wizard/demo01/server/service/SysUserService.java
@@ -0,0 +1,30 @@
+package com.wizard.demo01.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wizard.demo01.model.entity.SysUserEntity;
+import com.wizard.demo01.server.util.PageUtil;
+
+import java.util.Map;
+
+/**
+ * @author wizard_0992
+ * @date 2020/1/3 11:02
+ */
+
+public interface SysUserService extends IService<SysUserEntity> {
+
+    //修改密码
+    boolean updatePassword(Long userId,String oldPassword,String newPassword);
+
+    PageUtil queryPage(Map<String,Object> map);
+
+    void saveUser(SysUserEntity entity);
+
+    SysUserEntity getInfo(Long userId);
+
+    void updateUser(SysUserEntity entity);
+
+    void deleteUser(Long[] ids);
+
+    void updatePsd(Long[] ids);
+}
